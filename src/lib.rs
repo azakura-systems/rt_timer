2b81d47770215a5c4f2fe4cf37a0a7da9b446b15
Gentlegg <kosabalazs4@gmail.com>
2026-05-06 20:32:21 +0200

    init commit

---
+++ src/lib.rs
@@ -0,0 +1,48 @@
+use std::time::{Duration, Instant};
+
+#[derive(Debug, Clone)]
+pub struct Timer {
+    hz: u64,
+    dt: f64,
+    period: Duration,
+    next_tick: Instant,
+    log_accum: u64,
+}
+
+impl Timer {
+    pub fn new(hz: u64) -> Self {
+        let dt = 1.0 / hz as f64;
+        let period = Duration::from_secs_f64(dt);
+        Self {
+            hz,
+            dt,
+            period,
+            next_tick: Instant::now() + period,
+            log_accum: 0,
+        }
+    }
+
+    pub fn dt(&self) -> f64 {
+        self.dt
+    }
+
+    pub fn should_log(&mut self, hz: u64) -> bool {
+        self.log_accum += hz;
+        if self.log_accum >= self.hz {
+            self.log_accum -= self.hz;
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn wait(&mut self) {
+        let now = Instant::now();
+        if now < self.next_tick {
+            spin_sleep::sleep(self.next_tick - now);
+            self.next_tick += self.period;
+        } else {
+            self.next_tick = now + self.period;
+        }
+    }
+}
